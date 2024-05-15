3ca96c4eecfeb4a44781d4dd601aaacb9a721eaa
ServerLab002 <ServerLab002@DESKTOP-TN490BQ>
2024-05-15 12:12:14 -0600

    Agregar archivos de proyecto.

---
+++ SistemaMatriculaINUED/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnConectar = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnConectar
+        '
+        Me.btnConectar.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConectar.Image = Global.SistemaMatriculaINUED.My.Resources.Resources.down_
+        Me.btnConectar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnConectar.Location = New System.Drawing.Point(253, 24)
+        Me.btnConectar.Name = "btnConectar"
+        Me.btnConectar.Size = New System.Drawing.Size(311, 65)
+        Me.btnConectar.TabIndex = 0
+        Me.btnConectar.Text = "Realizar Conexión"
+        Me.btnConectar.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 105)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(776, 333)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Navy
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnConectar)
+        Me.Name = "Form1"
+        Me.Text = "Sistema Matrícula INUED - Maestros"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnConectar As Button
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ SistemaMatriculaINUED/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+    Private Sub btnConectar_Click(sender As Object, e As EventArgs) Handles btnConectar.Click
+        Dim conexion As New OleDbConnection("Provider=Microsoft.Jet.Oledb.4.0; Data Source=G:\1 CLASES 2024\PROGRAMACION 12C\II Parcial\SistemaMatriculaINUED\Base de Datos\matriculaINUED(2002-2003).mdb")
+        Try
+            conexion.Open()
+            MsgBox("Conectado")
+
+            Dim query = "Select * from maestro"
+            Dim adap As New OleDbDataAdapter(query, conexion)
+            Dim dt As New DataTable
+            adap.Fill(dt)
+            DataGridView1.DataSource = dt
+
+        Catch ex As Exception
+            MsgBox("No se conectó por: " & ex.Message)
+        End Try
+    End Sub
+End Class
